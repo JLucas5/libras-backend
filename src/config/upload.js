7a3179fbbce794b2ea70011f406721965dec09ed
João Lucas Pacheco A. Bastos <joaolucaspachecoab@gmail.com>
2020-02-04 16:01:11 -0300

    Image upload code created

---
+++ src/config/upload.js
@@ -0,0 +1,32 @@
+const multer = require('multer')
+const path = require('path')
+const aws = require('aws-sdk')
+const multerS3 = require('multer-s3')
+
+const storageTypes = {
+    local: multer.diskStorage({
+        destination: path.resolve(__dirname,'..', '..', 'uploads'),
+        filename: (req, file, cb) => {
+
+            const ext = path.extname(file.originalname)
+            const name = path.basename(file.originalname, ext)
+            file.key = `${name}-${Date.now()}${ext}`
+            cb(null, file.key)
+        }
+    }),
+    s3: multerS3({
+        s3: new aws.S3(),
+        bucket: 'foodsup-images',
+        contentType: multerS3.AUTO_CONTENT_TYPE,
+        acl: 'public-read',
+        key: (req, file, cb) => {
+            const ext = path.extname(file.originalname)
+            const name = path.basename(file.originalname, ext)
+            cb(null, `${name}-${Date.now()}${ext}`)
+        }
+    })
+}
+
+module.exports = {
+    storage: storageTypes['local']
+}
