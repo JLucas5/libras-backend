57fb4861b8d75f5f8ecaa2fa4430a3ceefe98a0b
João Lucas <joaolucaspachecoab@gmail.com>
2020-02-11 21:38:01 -0300

    Module Model updated

---
+++ src/Models/Module.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+const ModuleSchema = new mongoose.Schema({
+    name: String,
+    description: String,
+    hidden: Boolean
+}, 
+
+{
+    toJSON: {
+        virtuals: true,
+    }
+})
+
+module.exports = mongoose.model('Module', ModuleSchema)
