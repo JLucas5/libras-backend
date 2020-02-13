efb5a4c1ea411a913a32586e7f4d50695d3659d1
João Lucas <joaolucaspachecoab@gmail.com>
2020-02-13 17:09:28 -0300

    Enviroment variables checked

--- src/config/upload.js
+++ src/config/upload.js
@@ -16,12 +16,15 @@ const storageTypes = {
     }),
     s3: multerS3({
         s3: new aws.S3(),
-        bucket: 'libras-storage',
+        bucket: 'libras-file-storage',
         contentType: multerS3.AUTO_CONTENT_TYPE,
         acl: 'public-read',
+        
         key: (req, file, cb) => {
             const ext = path.extname(file.originalname)
+
             const name = path.basename(file.originalname, ext)
+
             cb(null, `${name}-${Date.now()}${ext}`)
         }
     })
