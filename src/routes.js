0ad9e0657145102d73888e0de552543d97a2b9da
João Lucas <joaolucaspachecoab@gmail.com>
2020-02-14 14:22:05 -0300

    Routes updated to reflect changes

--- src/routes.js
+++ src/routes.js
@@ -3,13 +3,22 @@ const multer = require('multer')
 const uploadConfig = require('./config/upload')
 
 const ModuleController = require('./Controllers/ModuleController')
-const activityController = require('./Controllers/ActivityController')
+const ActivityController = require('./Controllers/ActivityController')
+const DashboardController = require('./Controllers/DashboardController')
 
 const routes = express.Router()
 const upload = multer(uploadConfig)
 
-routes.post('/activities', upload.single('thumbnail'), activityController.create)
+routes.post('/activities', upload.single('thumbnail'), ActivityController.create)
 
-routes.post('/modules', ModuleController.store)
+routes.post('/activities/update/:activity_id', upload.single('thumbnail'), ActivityController.update)
+
+routes.post('/modules/new', ModuleController.store)
+
+routes.get('/modules', DashboardController.show)
+
+routes.delete('/modules/delete/:id', ModuleController.delete)
+
+routes.get('/modules/:module_id', ModuleController.show)
 
 module.exports = routes
