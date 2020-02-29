7ee8301f38320623c1d97a91831ee95f0380639b
João Lucas Pacheco A. Bastos <joaolucaspachecoab@gmail.com>
2020-02-29 02:48:45 -0300

    Routes updates

--- src/routes.js
+++ src/routes.js
@@ -12,19 +12,28 @@ const routes = express.Router()
 const upload = multer(uploadConfig)
 
 routes.post('/activities/new', upload.single('thumbnail'), ActivityController.create)
+routes.get('/activities/all/:module_id', ActivityController.all)
 routes.post('/activities/update/:activity_id', upload.single('thumbnail'), ActivityController.update)
-routes.delete('/activities/delete/:activity_id')
+routes.delete('/activities/delete/:activity_id', ActivityController.delete)
+
+routes.get('/modules', DashboardController.show)
+routes.post('/modules/new', upload.fields([{ name: 'thumbnail', maxCount: 1 }, { name: 'pdf', maxCount: 1 }]), ModuleController.store)
+routes.post('/modules/edit/:module_id',upload.fields([{ name: 'thumbnail', maxCount: 1 }, { name: 'pdf', maxCount: 1 }]), ModuleController.edit)
+routes.delete('/modules/delete/:module_id', ModuleController.delete)
 routes.get('/modules/:module_id', ModuleController.show)
 
-routes.post('/modules/new', upload.none(), ModuleController.store)
-routes.get('/modules', DashboardController.show)
-routes.delete('/modules/delete/:id', ModuleController.delete)
 
-routes.get('/library', LibraryController.show)
-routes.post('/library/new', upload.single('thumbnail'), LibraryController.store)
+routes.post('/library/new', upload.single('file'), LibraryController.store)
+routes.delete('/library/delete/:id', LibraryController.delete)
+routes.post('/library/view/:type', upload.none(), LibraryController.show)
+routes.post('/library/edit/:id', upload.single('file'), LibraryController.edit)
+routes.get('/library/:id', LibraryController.find)
+
 
 routes.get('/dictionary', DictionaryController.show)
 routes.post('/dictionary/new', upload.single('thumbnail'), DictionaryController.store)
-
+routes.get('/dictionary/:id', DictionaryController.find)
+routes.post('/dictionary/edit/:id', upload.single('thumbnail'), DictionaryController.edit)
+routes.delete('/dictionary/delete/:id', DictionaryController.delete)
 
 module.exports = routes
