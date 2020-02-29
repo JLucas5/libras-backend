ee24560cd8fa13f8e35e07887067186fd2153970
João Lucas Pacheco A. Bastos <joaolucaspachecoab@gmail.com>
2020-02-29 02:50:07 -0300

    Dictionary methods added

--- src/Controllers/DictionaryController.js
+++ src/Controllers/DictionaryController.js
@@ -1,32 +1,57 @@
 const Dictionary = require('../Models/Dictionary_Item')
 
+const mongoose = require('mongoose');
+const ObjectId = mongoose.Types.ObjectId;
+
 module.exports = {
+    
     async store(req, res){
         const { originalname, location } = req.file || {originalname: '', location: null}
         const { word } = req.body
 
-        const new_item = await Dictionary.create({
-            word,
-            location
-        })
-
+            const new_item = await Dictionary.create({
+                word,
+                location
+            })
         return res.json(new_item)
     },
 
     async show(req, res){
 
         const itemList = await Dictionary.find()
-        console.log(itemList)
 
         return res.json(itemList)
     },
 
     async delete(req, res){
 
-        const { item_id } = req.params
+        const { id } = req.params
 
-        Dictionary.findByIdAndDelete(item_id)
+        await Dictionary.findByIdAndDelete(id)
 
         return res.json({status: "Item deleted"})
+    },
+
+    async edit(req, res){
+
+        const { originalname, location } = req.file || {originalname: '', location: null}
+        const { word, old_image } = req.body
+        const { id } = req.params
+
+        const updated_item = await Dictionary.findByIdAndUpdate(id, {
+            word,
+            location: location ? location : old_image
+        })
+
+        return res.json(updated_item)
+    },
+
+    async find(req, res){
+
+        const { id } = req.params
+
+        const item = await Dictionary.findById(id)
+
+        return res.json(item)
     }
 }
--- src/Models/Dictionary_Item.js
+++ src/Models/Dictionary_Item.js
@@ -1,14 +1,9 @@
 const mongoose = require('mongoose')
 
 const DictionarySchema = new mongoose.Schema({
+
     word: String,
     location: String
-},
-
-{
-    toJSON: {
-        virtuals: true,
-    }
 })
 
 module.exports = mongoose.model('Dictionary', DictionarySchema)
